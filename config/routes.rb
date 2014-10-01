a03ac79410253d8bf236db490729b8e3229222d5
kz-kasai <rgb.cmyk.iroiro@gmail.com>
2014-10-01 14:55:07 +0900

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 SamuraiApp::Application.routes.draw do
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
 
 #  resources :microposts
-  resources :users
+#  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create,:destroy]
 
   root  'static_pages#home'
 
